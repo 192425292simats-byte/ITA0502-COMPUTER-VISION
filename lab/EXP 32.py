c3d0a2b847a1b6fea77d520c31e606023d9339fc
192425292simats-byte <192425292.simats@saveetha.com>
2026-08-13 16:47:49 +0530

    Update EXP 32.py

--- lab/EXP 32.py
+++ lab/EXP 32.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img=cv2.imread(r"C:\Users\Maruthi\Downloads\METAL.jpg",0)
+img=cv2.imread(r"C:\Users\sunil\Downloads\METAL.jpg",0)
 kernel=np.ones((5,5),np.uint8)
 closing=cv2.morphologyEx(img,cv2.MORPH_CLOSE,kernel)
 cv2.imshow("Original",img)
