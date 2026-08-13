ab82b55089b40b55407dedf9d36f5c2cf1c9a30d
192425292simats-byte <192425292.simats@saveetha.com>
2026-08-13 16:46:30 +0530

    Update video file path in EXP 37.py

--- lab/EXP 37.py
+++ lab/EXP 37.py
@@ -1,6 +1,6 @@
 import cv2
 
-cap=cv2.VideoCapture(r"C:\Users\Maruthi\Downloads\www.5MovieRulz.software - Love Oh Love (2026) 1080p TRUE WEB-DL - AVC - (DD+5.1 - 192Kbps) [Tel + Mal + Kan] - 2.6GB - ESub.mkv")
+cap=cv2.VideoCapture(r"C:\Users\sunil\Downloads\www.5MovieRulz.software - Love Oh Love (2026) 1080p TRUE WEB-DL - AVC - (DD+5.1 - 192Kbps) [Tel + Mal + Kan] - 2.6GB - ESub.mkv")
 
 frames=[]
 
