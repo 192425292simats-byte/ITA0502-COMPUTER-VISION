1961d5a998fae41f1ea35f35b2c765f59d85a6eb
192425292simats-byte <192425292.simats@saveetha.com>
2026-08-13 16:48:24 +0530

    Update EXP 30.py

--- lab/EXP 30.py
+++ lab/EXP 30.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread(r"C:\Users\Maruthi\Downloads\CLIMATE.png",0)
+img = cv2.imread(r"C:\Users\sunil\Downloads\CLIMATE.png",0)
 
 kernel = np.ones((5,5),np.uint8)
 
