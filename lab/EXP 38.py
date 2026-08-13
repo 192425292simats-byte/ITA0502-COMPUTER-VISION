2b07522abcae9b8a5d469481224f6790002f8633
192425292simats-byte <192425292.simats@saveetha.com>
2026-08-13 16:45:05 +0530

    Update EXP 38.py

--- lab/EXP 38.py
+++ lab/EXP 38.py
@@ -1,6 +1,6 @@
 import cv2
 
-img=cv2.imread(r"C:\Users\Maruthi\Downloads\METAL.jpg")
+img=cv2.imread(r"C:\Users\sunil\Downloads\METAL.jpg")
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 blur=cv2.GaussianBlur(gray,(5,5),0)
 faces=cv2.HoughCircles(blur,cv2.HOUGH_GRADIENT,1.2,50,param1=100,param2=50,minRadius=20,maxRadius=200)
