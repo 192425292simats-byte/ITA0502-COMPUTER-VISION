af32e4d7caeae0ff5fa928073196837974b05004
192425292simats-byte <192425292.simats@saveetha.com>
2026-08-13 16:47:02 +0530

    Update EXP 35.py

--- lab/EXP 35.py
+++ lab/EXP 35.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img=cv2.imread(r"C:\Users\Maruthi\Downloads\METAL.jpg",0)
+img=cv2.imread(r"C:\Users\sunil\Downloads\METAL.jpg",0)
 kernel=np.ones((5,5),np.uint8)
 blackhat=cv2.morphologyEx(img,cv2.MORPH_BLACKHAT,kernel)
 cv2.imshow("Original",img)
